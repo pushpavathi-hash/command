d04dd44ab4d23a953a75f540c5f6c18fdf7a00f4
pushpavathi-hash <63945353+pushpavathi-hash@users.noreply.github.com>
2024-11-14 09:07:36 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,43 @@
+a=10;
+b = 10;
+print(a*b);
+
+c=10.2;
+
+print(a*c)
+
+d=12.9
+
+print(c*d)
+
+e='11'
+
+#print(a+e) int and str cannot be added
+
+f='hello'
+
+# print(a+f) int and str cannot be added
+
+x="hi";
+y="hello";
+
+print(x+y)
+
+g=[1,2,3,4]
+
+#print(a + g) unsupported operand type(s) for +: 'int' and 'list'
+
+h=(1,2,3,5)
+
+#print(a+h) unsupported operand type(s) for +: 'int' and 'tuple'
+
+i=[6,7,8,9]
+
+print(i+g)
+#print(i+h) can only concatenate tuple (not "list") to tuple
+j=(9,4,5,6,3)
+
+print(h+j)
+
+# conclusion  Addition can happen only between same type 
+
